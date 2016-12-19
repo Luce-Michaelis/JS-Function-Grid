d44a3172b172f2e43c49e82a1b0daae4a2329ff4
Luce Michaelis <lucerorocks17@gmail.com>
2016-12-19 22:18:29 +0000

    I made a tree

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//Write your function calls below this line
+setColor(0, 5, "yellow");
+ setColor(1, 4, "green");
+ setColor(1, 5, "green");
+ setColor(1, 6, "green");
+ setColor(2, 3, "green");
+ setColor(2, 4, "green");
+ setColor(2, 5, "green");
+  setColor(2, 6, "green");
+  setColor(2, 7, "green");
+ setColor(3, 2, "green");
+ setColor(3, 3, "green");
+  setColor(3, 4, "green");
+ setColor(3, 5, "green");
+ setColor(3, 6, "green");
+  setColor(3, 7, "green");
+  setColor(3, 8, "green");
+ setColor(4, 5, "#663300");
